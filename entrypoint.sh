88e8a89d9abe711815989c31947b1bcc7dc339e4
vv13 <zwhvv13@gmail.com>
2020-06-01 18:43:41 +0800

    github async action

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -l
+
+SSHPATH="$HOME/.ssh"
+mkdir -p "$SSHPATH"
+echo "$DEPLOY_KEY" > "$SSHPATH/deploy_key"
+chmod 600 "$SSHPATH/deploy_key"
+sh -c "rsync $OPTION -e 'ssh -i ${SSHPATH}/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEST"
