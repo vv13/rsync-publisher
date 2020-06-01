8dd97429934f02c009c9d15dae263c714ba204ab
vv13 <zwhvv13@gmail.com>
2020-06-01 22:12:42 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 SSHPATH=${HOME}/.ssh
 mkdir -p $SSHPATH
-echo $DEPLOY_KEY > ${SSHPATH}/deploy_key
-chmod 600 ${SSHPATH}/deploy_key
+echo "$DEPLOY_KEY" > "${SSHPATH}/deploy_key"
+chmod 600 "${SSHPATH}/deploy_key"
 sh -c "rsync $OPTION -e 'ssh -i ${SSHPATH}/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEST"
